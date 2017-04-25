36cd560db372173faf618566b99dafd74483ecc5
Ernad619 <edo619@hotmail.com>
2017-04-25 12:16:02 +0200

    ..
    add comments when you change something

--- retrolution.cpp
+++ retrolution.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <windows.h>
-#define SIZE 999
+#include "string"
+#include "functions.h" // We should leave comments for each other in every new update :P
+// I put your #include "functions.h" please don't kill me xD
 
 using namespace std;
 
@@ -10,9 +12,20 @@ int main()
 {
     #include "variables.h"
 
-    cout << "This is Retrolution a small project made by Ernad619 and Ialeidioma," << endl;
+    cout << "TO PLAY THIS GAME PLEASE PUT THE CONSOLE ON HALF OF YOUR SCREEN AND TURN ON YOUR AUDIO\n";
+    cout << "AFTER YOU HAVE DONE THAT: ";
+    system("pause");
+    system("cls");
+    cout << "                                    -->HELLO<--\n"; // I also swapped every endl with \n xD
+    Sleep(2000);
+    cout << "                                 -->AND WELCOME<--\n";
+    Sleep(2000);
+    cout << "                               -->TO RETROLUTION<--\n";
+    Sleep(5000);
+    system("cls");
+    cout << "This is Retrolution a small project made by Ernad619 and Ialeidioma,\n";
     Sleep(1500);
-    cout << "if you are playing this the chances are you probably know us and know what this is all about," << endl;
+    cout << "if you are playing this the chances are you probably know us and know what this is all about,\n";
     Sleep(1500);
     cout << "but if you don't: this is a \"life simulator\" game where you take role of a character who has to manage his money and\nskill to accomplish";
     Sleep(1500);
@@ -21,18 +34,16 @@ int main()
         cout <<".";
     }
     Sleep(2000);
-    cout << "well that's up to you to accomplish..." << endl;
+    cout << "well that's up to you to accomplish...\n";
     Sleep(700);
     line();
-    cout <<"Please enter your name to continue"<< endl;
+    cout <<"Please enter your name to continue\n";
     getline(cin,player_name);
     cout << "Hello, " << player_name << endl;
     Sleep(700);
-    cout << "ZzZzZzZ..." << endl;
+    cout << "ZzZzZzZ...\n";
     Sleep(700);
-    cout << "ZzZzzZ..." << endl;
-    cout << "\a" << endl;
+    cout << "ZzZzzZ...\n";
+    cout << "\a\n";
     return 0;
 }
-
-#include "functions.h"
--- variables.h
+++ variables.h
@@ -1,2 +1,8 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H // These are include guards they check if it has been defined earlier and prevent double declaration of identifiers such as types enumerators and static variables
+#include "string"
+
 int i;
-string player_name;
+std::string player_name;
+
+#endif
